0d1e11e98dcf92bacbe65ce572e042f9457675de
qs <ease_lee@163.com>
2025-04-12 00:17:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def get_douban_hot_movies():
     """
     从豆瓣获取当前热映电影信息
     """
-    url = "https://movie.douban.com/cinema/nowplaying/"
+    url = "https://movie.douban.com/cinema/nowplaying/qinhuangdao/"
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
     }
