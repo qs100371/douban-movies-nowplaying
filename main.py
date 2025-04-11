93e467d0030bc400ccdbf7d3ce3b70613f7f9754
qs <ease_lee@163.com>
2025-04-11 16:23:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -70,6 +70,7 @@ def generate_html(movies):
     <html lang="zh-CN">
     <head>
         <meta charset="UTF-8">
+        <meta name="referrer" content="no-referrer">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>豆瓣热映电影</title>
         <style>
